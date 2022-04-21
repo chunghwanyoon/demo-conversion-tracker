81eea9961c297c6defbf407150d7977c628062fe
Chunghwan <chunghwan@redrocket.co.kr>
2022-04-21 18:02:15 +0900

    fixes

--- spec/controllers/auth_controller_spec.rb
+++ spec/controllers/auth_controller_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe AuthController, type: :controller do
   describe "POST /api/v1/sign_up" do
-    it "this is how ther fuckaslkdfjdsaklf" do
-      post :sign_up, params: { account_name: "blahblahbalh", password: "testisSuck" }
+    it "signup route test" do
+      post :sign_up, params: { account_name: "blahblahbalh", password: "testck" }
       puts response
       puts response.body
       expect(response).to be_successful
